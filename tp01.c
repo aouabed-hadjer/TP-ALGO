e4861da315908c9312f9cb35e74c148d02b878be
aouabed-hadjer <aouabedhadjer39@gmail.com>
2025-10-19 15:12:55 +0100

    Implement matrix input and transposition
    Added a program to input and transpose a square matrix.

--- tp01.c
+++ tp01.c
@@ -1,2 +1,37 @@
-t(t(t''f'f'
-gggg
+#include<stdio.h>
+int main (){
+  int n;
+  int arr [n][n],i,j;
+  int h = arr [i][j];
+    printf("enter size of the square matrix :");
+      scanf("%d",&n);
+
+    printf("enter the elements of the matrix :\n");
+       for(i=0 ;i<n ;i++){
+         for(j=0 ;j<n ;j++){
+           scanf("%d",&arr [i][j]);
+                                      }
+                                          }
+    printf("the matrix before :\n");
+       for(i=0 ;i<n ;i++){
+         for(j=0 ;j<n ;j++){
+           scanf("%d",&arr[i][j]);
+                                      }
+         printf("\n");
+                                          }
+
+       for(i=0 ;i<n ;i++){
+         for(j=i+1 ;j<n ;j++){
+            arr[i][j]=arr[j][i];
+            arr[i][j]=h;
+                                      }        
+                                          }
+    printf("the matrix after :\n");
+       for(i=0 ;i<n ;i++){
+         for(j=0 ;j<n ;j++){
+           scanf("%d",&arr[i][j]);
+                                      }
+         printf("\n");
+                                          }
+return 0;
+                                                   }
